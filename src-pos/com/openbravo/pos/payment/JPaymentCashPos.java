b21269d05a42a8e0f1ee3b597f5eaa147a0f68d2
RiffRaff <office@rammelhof.at>
2016-01-19 22:39:21 +0100

    Set default size for products and categories;

--- src-pos/com/openbravo/pos/payment/JPaymentCashPos.java
+++ src-pos/com/openbravo/pos/payment/JPaymentCashPos.java
@@ -149,7 +149,7 @@ public class JPaymentCashPos extends javax.swing.JPanel implements JPaymentInter
 		public void addButton(String image, double amount) {
 			JButton btn = new JButton();
 			btn.setIcon(new ImageIcon(tnbbutton.getThumbNailText(dlSystem.getResourceAsImage(image),
-					Formats.CURRENCY.formatValue(amount), 24)));
+					Formats.CURRENCY.formatValue(amount), 20)));
 			btn.setFocusPainted(false);
 			btn.setFocusable(false);
 			btn.setRequestFocusEnabled(false);
