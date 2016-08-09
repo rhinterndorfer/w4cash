74a7066cce1892b3661bf6d9d799ffb1188a1fd1
Hannes <h.bichler@hb-softsolution.com>
2016-08-09 15:01:09 +0200

    Add single line for each Ein-, Auszahlung and add Comment in Kassenabschluss. Add net value to each tax line in CloseCash and PartialCash

--- src-pos/com/openbravo/pos/panels/PaymentsModel.java
+++ src-pos/com/openbravo/pos/panels/PaymentsModel.java
@@ -43,7 +43,7 @@ public class PaymentsModel {
     private Double m_dPaymentsTotal;
     private java.util.List<PaymentsLine> m_lpayments;
     
-    private final static String[] PAYMENTHEADERS = {"Label.Payment", "label.totalcash"};
+    private final static String[] PAYMENTHEADERS = {"Label.Payment", "Label.Description", "label.totalcash"};
     
     private Integer m_iSales;
     private Double m_dSalesBase;
@@ -99,10 +99,11 @@ public class PaymentsModel {
         }  
         
         List l = new StaticSentence(app.getSession()            
-            , "SELECT PAYMENTS.PAYMENT, SUM(PAYMENTS.TOTAL) " +
+            //, "SELECT PAYMENTS.PAYMENT, SUM(PAYMENTS.TOTAL) " +
+            , "SELECT PAYMENTS.PAYMENT, SUM(PAYMENTS.TOTAL), PAYMENTS.DESCRIPTION " +
               "FROM PAYMENTS, RECEIPTS " +
               "WHERE PAYMENTS.RECEIPT = RECEIPTS.ID AND RECEIPTS.MONEY = ? " +
-              "GROUP BY PAYMENTS.PAYMENT"
+              "GROUP BY PAYMENTS.DESCRIPTION, PAYMENTS.PAYMENT"
             , SerializerWriteString.INSTANCE
             , new SerializerReadClass(PaymentsModel.PaymentsLine.class)) //new SerializerReadBasic(new Datas[] {Datas.STRING, Datas.DOUBLE}))
             .list(app.getActiveCashIndex(false)); 
@@ -142,7 +143,7 @@ public class PaymentsModel {
         } 
                 
         List<SalesLine> asales = new StaticSentence(app.getSession(),
-                "SELECT TAXCATEGORIES.NAME, SUM(TAXLINES.AMOUNT) " +
+                "SELECT TAXCATEGORIES.NAME, SUM(TAXLINES.AMOUNT),  SUM(TAXLINES.BASE) " +
                 "FROM RECEIPTS, TAXLINES, TAXES, TAXCATEGORIES WHERE RECEIPTS.ID = TAXLINES.RECEIPT AND TAXLINES.TAXID = TAXES.ID AND TAXES.CATEGORY = TAXCATEGORIES.ID " +
                 "AND RECEIPTS.MONEY = ? " +
                 "GROUP BY TAXCATEGORIES.NAME"
@@ -251,7 +252,12 @@ public class PaymentsModel {
                 PaymentsLine l = m_lpayments.get(row);
                 switch (column) {
                 case 0: return l.getType();
-                case 1: return l.getValue();
+                case 1:
+                	if(l.getDescription() == null)
+                		return "";
+                	else
+                		return l.getDescription();
+                case 2: return l.getValue();
                 default: return null;
                 }
             }  
@@ -262,10 +268,12 @@ public class PaymentsModel {
         
         private String m_SalesTaxName;
         private Double m_SalesTaxes;
+        private Double m_SalesNet;
         
         public void readValues(DataRead dr) throws BasicException {
             m_SalesTaxName = dr.getString(1);
             m_SalesTaxes = dr.getDouble(2);
+            m_SalesNet = dr.getDouble(3);
         }
         public String printTaxName() {
             return m_SalesTaxName;
@@ -278,7 +286,14 @@ public class PaymentsModel {
         }
         public Double getTaxes() {
             return m_SalesTaxes;
-        }        
+        }
+        
+        public Double getSalesNet() {
+            return m_SalesNet;
+        }
+        public String printNet() {
+            return Formats.CURRENCY.formatValue(m_SalesNet);
+        }
     }
 
     public AbstractTableModel getSalesModel() {
@@ -306,18 +321,26 @@ public class PaymentsModel {
     public static class PaymentsLine implements SerializableRead {
         
         private String m_PaymentType;
+        private String m_PaymentDescription;
         private Double m_PaymentValue;
         
         public void readValues(DataRead dr) throws BasicException {
             m_PaymentType = dr.getString(1);
             m_PaymentValue = dr.getDouble(2);
+            m_PaymentDescription = dr.getString(3);
         }
         
         public String printType() {
             return AppLocal.getIntString("transpayment." + m_PaymentType);
         }
+        public String printDescription() {
+            return m_PaymentDescription == null ? "" : m_PaymentDescription;
+        }
         public String getType() {
             return m_PaymentType;
+        }
+        public String getDescription() {
+            return m_PaymentDescription;
         }
         public String printValue() {
             return Formats.CURRENCY.formatValue(m_PaymentValue);
