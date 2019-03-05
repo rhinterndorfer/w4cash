653d8b54c61818aa95d05ca9ad97012f5cfd6d02
Raphael Hinterndorfer <raphael.hinterndorfer@outlook.at>
2019-03-05 17:22:15 +0100

    Deactivate license check;

--- src-pos/com/openbravo/license/DeviceInfo.java
+++ src-pos/com/openbravo/license/DeviceInfo.java
@@ -91,9 +91,9 @@ public class DeviceInfo {
 	}
 
 	public boolean validateLicense(String currentLizense) {
-		if (currentLizense == null || this.m_License == null || !(currentLizense.equals(this.m_License) || "1111-2222".equals(this.m_License))) {
-			return false;
-		}
+		//if (currentLizense == null || this.m_License == null || !(currentLizense.equals(this.m_License) || "1111-2222".equals(this.m_License))) {
+		//	return false;
+		//}
 		return true;
 	}
 }
